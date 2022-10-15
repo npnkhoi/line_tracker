5f08cd4923f3ec4d81fc3d3c6a72f1aa13000624
Phuong Nghi <phuongnghi120803@gmail.com>
2022-10-15 15:50:46 +0700

    Nghi 15.10

--- UltrasonicSensor.h
+++ UltrasonicSensor.h
@@ -7,6 +7,7 @@ class UltrasonicSensor {
   public:
     UltrasonicSensor(int trig, int echo);
     float getDist();
+    bool check();
   private:
     int _trig, _echo;
 };
--- line_tracker.ino
+++ line_tracker.ino
@@ -7,7 +7,7 @@ int speed = 200;
 const int IR_PINS[] = {8, 9, 10, 11, 12};
 int Kp = 75; //thay đổi theo 
 int MAX_SPEED = 255;
-//int mode[] = {0,1,2};
+int mode[] = {0,1,2};
 
 /*
  * MODE
@@ -19,8 +19,8 @@ int MAX_SPEED = 255;
 // Declare the devices
 Motor motor(4, 5, 7, 6);
 UltrasonicSensor usLeft(A0, A1);
-UltrasonicSensor usMiddle(A2, A3);
-UltrasonicSensor usRight(A4, A5);
+UltrasonicSensor usRight(A2, A3);
+UltrasonicSensor usSide(A4, A5);
 IRSensor irSensor(IR_PINS);
 
 
@@ -36,14 +36,13 @@ void setup()
 void loop()
 { 
   //Test IR middle
-  /*
-  if ((mode == 0) & ((usLeft.check()) || (usRight.check())) { //if on-line and detect obstacle
-    mode = 1;
+  if ((mode == 0) & ((usLeft.check()) || (usRight.check()))){ //if on-line and detect obstacle
+    int mode = 1;
     while ((usLeft.check()) & (usRight.check())) { //assuming side sensor on the right
-       motor.turnRight();
+      motor.turnRight(speed);
     }
-  } else if ((mode == 1) & (!usLeft.check()) & (!usRight.check()) & (irSenSor.getError != 4)) { 
-    mode = 0;
+  } else if ((mode == 1) & (!usLeft.check()) & (!usRight.check()) & (irSensor.getError() != 4)) { 
+    int mode = 0;
   }
 
   if (mode == 0) {
@@ -57,7 +56,7 @@ void loop()
          if (error != 4)  {//inline
              motor.go(speedLeft, speedRight);
          } else {//outline
-             mode = 2;
+             int mode = 2;
              }
          }
       else {
@@ -69,17 +68,17 @@ void loop()
     while (!usLeft.check()) {
       motor.go(speed - Kp, speed + Kp);
     }
-    while (usLeft,check()) {
+    while (usLeft.check()) {
       motor.go(speed + Kp, speed - Kp);
     }
     motor.stop();
-  } */
+  } 
   Serial.print("Left: ");
   Serial.println(usLeft.getDist());
-  Serial.print("Middle: ");
-  Serial.println(usMiddle.getDist());
   Serial.print("Right: ");
   Serial.println(usRight.getDist());
+  Serial.print("Side: ");
+  Serial.println(usSide.getDist());
   delay(1000);
   Serial.println("---------------");
 }  
